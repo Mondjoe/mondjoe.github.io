9fac245ac6df7538cc1b64a66afb4da885a42c53
Charm <kyawzayya4788@gmail.com>
2026-06-06 08:25:24 +0400

    Add multiple HTML files to service worker cache

--- sw.js
+++ sw.js
@@ -1,26 +1,14 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("charm-cache").then((cache) => {
-      return cache.addAll(["/offline.html"]);
+      return cache.addAll([
+        "/offline.html",
+        "/index.html",
+        "/dashboard.html",
+        "/about.html",
+        "/settings.html"
+      ]);
     })
   );
   self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    clients.claim().then(() => {
-      return self.clients.matchAll({ type: "window" }).then((clients) => {
-        clients.forEach((client) => {
-          client.postMessage({ type: "NEW_VERSION" });
-        });
-      });
-    })
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match("/offline.html"))
-  );
 });
