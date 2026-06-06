fce7f02e9f60f28e072fd64be2daae0000f10497
Charm <kyawzayya4788@gmail.com>
2026-06-06 08:28:06 +0400

    Add service worker event listeners for activation and fetch

--- sw.js
+++ sw.js
@@ -12,3 +12,26 @@ self.addEventListener("install", (event) => {
   );
   self.skipWaiting();
 });
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    clients.claim().then(() => {
+      return self.clients.matchAll({ type: "window" }).then((clients) => {
+        clients.forEach((client) => {
+          client.postMessage({ type: "NEW_VERSION" });
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      if (event.request.mode === "navigate") {
+        return caches.match("/offline.html");
+      }
+      return caches.match(event.request);
+    })
+  );
+});
